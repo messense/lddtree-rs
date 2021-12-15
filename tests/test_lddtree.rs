3d090a561b5c916d08404e40c02ce265cdec7a62
messense <messense@icloud.com>
2021-12-16 00:26:53 +0800

    Add a test case for DependencyAnalyzer

---
+++ tests/test_lddtree.rs
@@ -0,0 +1,22 @@
+use lddtree::DependencyAnalyzer;
+
+#[test]
+fn test_lddtree() {
+    let analyzer = DependencyAnalyzer::new();
+    let deps = analyzer.analyze("tests/test.elf").unwrap();
+    assert_eq!(
+        deps.interpreter.as_deref(),
+        Some("/lib/ld-linux-aarch64.so.1")
+    );
+    assert_eq!(
+        deps.needed,
+        &[
+            "libz.so.1",
+            "libpthread.so.0",
+            "libm.so.6",
+            "libdl.so.2",
+            "libc.so.6",
+        ]
+    );
+    assert_eq!(deps.libraries.len(), 6);
+}
