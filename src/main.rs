64d72c397f4f7a23adf95a69e47a0a5334b2340e
messense <messense@icloud.com>
2021-12-23 16:29:16 +0800

    Better cli output

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use std::collections::HashMap;
 use std::env;
 use std::error::Error;
 use std::path::PathBuf;
 use std::process;
 
-use lddtree::DependencyAnalyzer;
+use lddtree::{DependencyAnalyzer, Library};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut args = env::args().skip(1);
@@ -14,10 +15,37 @@ fn main() -> Result<(), Box<dyn Error>> {
             .unwrap_or_else(|| PathBuf::from("/"));
         let analyzer = DependencyAnalyzer::new(root);
         let deps = analyzer.analyze(pathname)?;
-        println!("{:#?}", deps);
+        if let Some(interp) = deps.interpreter {
+            if let Some(path) = deps
+                .libraries
+                .get(&interp)
+                .and_then(|lib| lib.realpath.as_ref())
+            {
+                println!("{} => {}", interp, path.display());
+            } else {
+                println!("{}", interp);
+            }
+        }
+        for needed in deps.needed {
+            print_library(&needed, &deps.libraries, 0);
+        }
     } else {
         eprintln!("USAGE: lddtree <pathname> [root]");
         process::exit(1);
     }
     Ok(())
+}
+
+fn print_library(name: &str, libraries: &HashMap<String, Library>, level: usize) {
+    let padding = " ".repeat(level);
+    if let Some(lib) = libraries.get(name) {
+        if let Some(path) = lib.realpath.as_ref() {
+            println!("{}{} => {}", padding, name, path.display());
+        } else {
+            println!("{}{}", padding, name);
+        }
+        for needed in &lib.needed {
+            print_library(needed, libraries, level + 4);
+        }
+    }
 }
